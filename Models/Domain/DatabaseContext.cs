c595ea9ccf97f2430d851b90618571ce33b8dad9
AbhijitSquareGroup <138113662+abhijitkghosh@users.noreply.github.com>
2024-03-02 12:39:45 +0600

    Movie (Create Edit and  Movie List)

--- Models/Domain/DatabaseContext.cs
+++ Models/Domain/DatabaseContext.cs
@@ -5,12 +5,13 @@ namespace MovieStoreMvc.Models.Domain
 {
     public class DatabaseContext : IdentityDbContext<ApplicationUser>
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext>options) : base(options)
+        public DatabaseContext (DbContextOptions<DatabaseContext> options) : base(options)
         {
-            
+
         }
+
         public DbSet<Genre> Genre { get; set; }
         public DbSet<MovieGenre> MovieGenre { get; set; }
-        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Movie> Movie { get; set; }
     }
 }
